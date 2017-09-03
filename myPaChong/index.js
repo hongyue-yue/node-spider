a8bc413a03a82a4c958a27391fa913f3189746d8
qinyueshang <819937282@qq.com>
2017-09-03 22:16:12 +0800

    modified file

--- myPaChong/index.js
+++ myPaChong/index.js
@@ -1,7 +1,7 @@
 
 const requestLagou=require('./getData');
 
-let city=["北京","上海","广州","深圳","杭州","南京","成都","西安","武汉","重庆"];
+let city=["上海"];
 let position=["web","java","php","android","c++","python",".NET"];
 
 if(process.argv.length === 4&&city.includes(process.argv[2])&&position.includes(process.argv[3])){
@@ -9,6 +9,5 @@ if(process.argv.length === 4&&city.includes(process.argv[2])&&position.includes(
   requestLagou.getData(process.argv[2], process.argv[3]);
 }else{
   console.log('请正确输入要爬取的城市和职位,正确格式为 node index 城市 职位，如 node index 上海 前端')
-  console.log('城市如下：北京,上海,广州,深圳,杭州,南京,成都,西安,武汉,重庆')
   console.log('职位如下：web,java,php,android,c++,python,.NET')
 }
